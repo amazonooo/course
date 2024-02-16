8ccade4350da6ae7102c8f46536d3d408975eef0
GrigorUN <155751051+amazonooo@users.noreply.github.com>
2024-02-16 11:49:55 +0300

    df

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+// function getStatus(age) {
+//     if(age < 14) return 'Child'
+//     if(age < 20) return 'Teen'
+//     if(age < 50) return 'Adult'
+
+//     return 'Old'
+// }
+
+// console.log(getStatus(17));
+
 function getStatus(age) {
-    if(age < 14) return 'Child'
-    if(age < 20) return 'Teen'
-    if(age < 50) return 'Adult'
-
-    return 'Old'
+    switch(age) {
+        case age < 14:
+            return 'Child';
+        case age < 20:
+            return 'teen';
+        case age < 50:
+            return 'adult';
+        default:
+            return 'Old'
+    }
 }
-
-console.log(getStatus(17));
