9ead1dd7bbe93ec71ff63188331e8c5ce8580076
amazonooo <garikogannisyan.9@gmail.com>
2024-03-17 12:50:10 +0300

    1:41

--- 01-base/app.js
+++ 01-base/app.js
@@ -15,12 +15,34 @@ intMinus.addEventListener('click', () => {
     action = '-'
 })
 
+function printResult(result) {
+     if (result < 0) {
+		resultElement.style.color = 'red'
+	} else {
+		resultElement.style.color = 'green'
+	}
+    resultElement.textContent = result
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    // if (actionSymbol === '+') {
+    //     return num1+ num2
+    // } else if (actionSymbol === '-') {
+    //     return num1 - num2
+    // }
+    return actionSymbol === '+' ? num1 + num2 : num1 - num2
+}
+
 onSubmit.addEventListener('click', () => {
-    if (action === '+') {
-        const sum = Number(input1.value) + Number(input2.value);
-        resultElement.textContent = sum
-    } else if (action === '-') {
-        const sum = Number(input1.value) - Number(input2.value)
-		resultElement.textContent = sum
-    }
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
+    // if (action === '+') {
+    //     const sum = Number(input1.value) + Number(input2.value);
+    //     printResult(sum)
+    // } else if (action === '-') {
+    //     const sum = Number(input1.value) - Number(input2.value)
+    //     printResult(sum)
+    // }
 })
